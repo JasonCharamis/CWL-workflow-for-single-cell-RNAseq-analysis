47cb5bacbb4f7df9d3df067963f3b3e1d1985a7d
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2024-01-12 12:01:34 +0200

    Update sce_analysis.R

--- sce_analysis.R
+++ sce_analysis.R
@@ -10,11 +10,7 @@
 
 ## Load libraries
 suppressPackageStartupMessages(library(seurat))
-suppressPackageStartupMessages(library(scater))
-suppressPackageStartupMessages(library(scran))
-suppressPackageStartupMessages(library(bluster))
 suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(DropletUtils))
 
 
 ## Get input file from standard input ------------------------------------------------------------------
@@ -22,38 +18,32 @@ args <- commandArgs(trailingOnly = TRUE)
 stopifnot(length(args) > 0, file.exists(args))
 
 ## Read file as SingleCellExperimentClass
-sce <- read10xCounts(args[1])
+sce <- Read10X(args[1])
 
 # Quality control based on MT genes percent
-is.mito <- grepl("^MT-", rownames(sce))
-qcstats <- perCellQCMetrics(sce, subsets=list(Mito=is.mito))
-filtered <- quickPerCellQC(qcstats, percent_subsets="subsets_Mito_percent")
+mito_genes <- grep("^MT-", rownames(sce), value = TRUE)
+sce <- AddMetaData(sce, metadata = perCellQCMetrics(sce, subsets = list(Mito = mito_genes)))
 
 # Normalization
-set.seed(1234)
-clusters <- quickCluster(sce)
-sce <- computeSumFactors(sce, cluster=clusters) 
-sce <- logNormCounts(sce)
+sce <- NormalizeData(sce)
 
 # Feature selection
-dec <- modelGeneVar(sce)
-hvg <- getTopHVGs(dec, prop=0.1)
+sce <- FindVariableFeatures(sce, selection.method = "vst", nfeatures = 2000)
 
-# PCA with 25 components (default)
-set.seed(1234)
-sce <- runPCA(sce, ncomponents=25, subset_row=hvg)
-ggsave(plot=sce,"PCA.svg", dpi = 1000)
+# PCA
+sce <- RunPCA(sce, features = VariableFeatures(object = sce), npcs = 25)
 
 # Clustering
-snn.gr <- buildSNNGraph(sce, use.dimred="PCA")
-colLabels(sce) <- factor(igraph::cluster_walktrap(snn.gr)$membership)
+sce <- FindNeighbors(sce, dims = 1:25)
+sce <- FindClusters(sce, resolution = 0.5)
 
-tSNE <- plotTSNE(sce, colour_by="label")
-UMAP <- plotUMAP(sce, colour_by="label")
+# tSNE and UMAP
+tSNE <- RunTSNE(sce, dims = 1:25)
+UMAP <- RunUMAP(sce, dims = 1:25)
 
 ggsave(plot=tSNE,"tSNE.svg", dpi = 1000)
 ggsave(plot=UMAP,"UMAP.svg", dpi = 1000)
 
-## Marker gene detection
-markers <- findMarkers(sce, direction="up")
-write.table("Marker_genes_detected.txt", markers, quote = F)
+# Marker gene detection
+markers <- FindAllMarkers(sce, only.pos = TRUE, min.pct = 0.25)
+write.table(markers, file = "Marker_genes_detected.txt", quote = FALSE, sep = "\t")
